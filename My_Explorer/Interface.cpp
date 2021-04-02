bbd7a5b307d378c6b1ee308d82f26fae058b77ac
Iluhap <iluhaplaksa@gmail.com>
2021-04-03 01:07:49 +0300

    Added initializing subitems to Listview

--- My_Explorer/Interface.cpp
+++ My_Explorer/Interface.cpp
@@ -5,7 +5,6 @@
 
 #include "resource.h"
 
-// #define _WIN32_IE 0x0400
 #include "commctrl.h"
 
 // Method used to interact with List-View window //
@@ -31,29 +30,28 @@ inline void FolderView::CreateListView(HWND hwndParent, HINSTANCE hInst)
 
 inline bool FolderView::InitListViewColumns()
 {
-	LVCOLUMN lvc;
-	int iCol = 0;
-
 	// Initialize the LVCOLUMN structure.
 	// The mask specifies that the format, width, text,
 	// and subitem members of the structure are valid.
+
+	LVCOLUMN lvc;
+
 	lvc.mask = LVCF_FMT | LVCF_WIDTH | LVCF_TEXT | LVCF_SUBITEM;
+	lvc.cx = 150;	// Width of column in pixels.
+	lvc.fmt = LVCFMT_LEFT;  // Left-aligned column
+
+	int iCol = 0;
 
 	// Add the columns.
 	for (std::string column : this->columns)
 	{
 		lvc.iSubItem = iCol;
 		lvc.pszText = (LPSTR)(column.c_str());
-		lvc.cx = 100;               // Width of column in pixels.
 
-			lvc.fmt = LVCFMT_LEFT;  // Left-aligned column.
-			// lvc.fmt = LVCFMT_RIGHT; // Right-aligned column.
-
-		// Insert the columns into the list view.
+	// Insert the columns into the list view.
 		if (ListView_InsertColumn(this->hListBox, iCol++, &lvc) == -1)
 			return false;
 	}
-
 	return true;
 }
 
@@ -61,20 +59,21 @@ inline bool FolderView::InsertListViewItems() // TODO implement adding other inf
 {
 	LVITEM lvI;
 
+	unsigned index = 0;
+
 	// Initialize LVITEM members that are common to all items.
 	lvI.pszText = LPSTR_TEXTCALLBACK; // Sends an LVN_GETDISPINFO message.
 	lvI.mask = LVIF_TEXT | LVIF_IMAGE | LVIF_STATE;
 	lvI.stateMask = 0;
-	lvI.iSubItem = 3;
+	lvI.iSubItem = 0;
 	lvI.state = 0;
 
 	std::vector<std::string> dirs = this->currDir->getDirs(); // List of directories
 
 	std::vector<File> files = this->currDir->getFiles(); // List of files
 
-	size_t index = 0;
+	 // Initialize LVITEM members that are different for each item.
 
-	// Initialize LVITEM members that are different for each item.
 	for (std::string dirName : dirs)
 	{
 		lvI.iItem = index;
@@ -85,6 +84,8 @@ inline bool FolderView::InsertListViewItems() // TODO implement adding other inf
 		// Insert items into the list.
 		if (ListView_InsertItem(this->hListBox, &lvI) == -1)
 			return false;
+
+		// ListView_SetItemText(this->hListBox, index, 1, (LPSTR)"Folder");
 
 		index++;
 	}
@@ -99,6 +100,13 @@ inline bool FolderView::InsertListViewItems() // TODO implement adding other inf
 		// Insert items into the list.
 		if (ListView_InsertItem(this->hListBox, &lvI) == -1)
 			return false;
+
+		ListView_SetItemText(this->hListBox, index, 1, (LPSTR)file.extension.c_str());
+
+		char date[30];
+		wsprintf(date, "%d.%d.%d", file.time.wDay, file.time.wMonth, file.time.wYear); // TODO find out better way to concatenate date
+		
+		ListView_SetItemText(this->hListBox, index, 2, (LPSTR)(date));
 
 		index++;
 	}
@@ -115,11 +123,8 @@ inline FolderView::FolderView(Directory* dir, const RECT& cRect)
 	setRect(RECT{ (int)round(cRect.right * 0.3), cRect.top, cRect.right, cRect.bottom });
 }
 
-inline void FolderView::Create(HWND hWnd ,HINSTANCE hInst)
+inline void FolderView::Create(HWND hWnd, HINSTANCE hInst)
 {
-
-	// TODO Implement creating of list view
-
 	CreateListView(hWnd, hInst);
 
 	InitListViewColumns();
@@ -131,7 +136,6 @@ inline void FolderView::Create(HWND hWnd ,HINSTANCE hInst)
 inline void FolderView::setRect(const RECT& rect)
 {
 	this->area = rect;
-	// TODO implement changing size of List-View
 }
 
 inline void FolderView::setDir(Directory& directory)
